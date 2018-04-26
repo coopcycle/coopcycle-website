99662ac0c64f8fcfc828a503aa295a1ea01e6857
Édith Darin <edith.darin@data.gouv.fr>
2018-04-26 17:24:34 +0200

    an attempt for inclusion of a counter

---
+++ static/additional_widget.js
@@ -0,0 +1,47 @@
+function isElementInViewport (el) {
+    
+        //special bonus for those using jQuery
+        if (typeof jQuery === "function" && el instanceof jQuery) {
+            el = el[0];
+        }
+    
+        var rect = el.getBoundingClientRect();
+    
+        return (
+            rect.top >= 0 &&
+            rect.left >= 0 &&
+            rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) && /*or $(window).height() */
+            rect.right <= (window.innerWidth || document.documentElement.clientWidth) /*or $(window).width() */
+        );
+    }
+function onVisibilityChange(el, callback) {
+    var old_visible;
+    return function () {
+        var visible = isElementInViewport(el);
+        if (visible != old_visible) {
+            old_visible = visible;
+            if (typeof callback == 'function') {
+                callback();
+            }
+        }
+    }
+}
+    
+var handler = onVisibilityChange(el, function() {
+    $('.count-cotis').each(function () {
+        $(this).prop('Counter',0).animate({
+            Counter: $(this).text()
+        }, {
+            duration:3000,
+            easing: 'swing',
+            step: function (now) {
+                $(this).text(Math.ceil(now));
+            }
+        });
+    });
+});
+
+$(window).on('DOMContentLoaded load resize scroll', handler); 
+
+        
+
