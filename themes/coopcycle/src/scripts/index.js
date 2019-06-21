345a9b58d51a85cbc3faae0e41ff791f1fb9a641
Atala <alois.guillope@gmail.com>
2019-06-21 14:32:03 +0200

    feat: new style for coops display on network page

--- themes/coopcycle/src/scripts/index.js
+++ themes/coopcycle/src/scripts/index.js
@@ -15,44 +15,70 @@ function fadeIn(el) {
 }
 
 let SmoothScroll = require('smooth-scroll')
-new SmoothScroll('a[href*="#"]', {speed: 700})
+new SmoothScroll('a[href*="#"]', {
+  speed: 700
+})
 
 if (document.getElementById('coops-map')) {
-    let L = require('leaflet'),
-        markers = [],
-        marker
+  let L = require('leaflet'),
+    markers = [],
+    marker
 
-    var map = L.map('coops-map')
+  var map = L.map('coops-map')
 
-    L.tileLayer('https://cartodb-basemaps-{s}.global.ssl.fastly.net/rastertiles/voyager/{z}/{x}/{y}.png', {
-        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>, &copy;<a href="https://carto.com/attribution">CARTO</a>'
-    }).addTo(map)
+  L.tileLayer('https://cartodb-basemaps-{s}.global.ssl.fastly.net/rastertiles/voyager/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>, &copy;<a href="https://carto.com/attribution">CARTO</a>'
+  }).addTo(map)
 
-    window.AppData.coops.forEach(
-        function (item) {
-            marker = L.marker([item.latitude, item.longitude]).addTo(map)
-            marker.bindPopup(
-              `
-                ${item.name ? item.name : item.city}<br>
-                ${item.coopcycleUrl ? `<a class="btn-buy" href="${item.coopcycle_url}" target="_blank">${window.AppData.i18n.orderNow}</a>` : ''}
-                ${item.url ? `<a href="${item.url}" target="_blank">${window.AppData.i18n.visit}</a>` : ''}
-              `
-            )
-            markers.push(marker)
-        }
-    )
+  window.AppData.coops.forEach(
+    function(item) {
+      marker = L.marker([item.latitude, item.longitude]).addTo(map).on('click', function() {
+        // fill in modal with coop data
+        let text = item.text ? (item.text[window.AppData.lang] || item.text['en']) : ''
+        $('#coop-modal .modal-content').html(`
+            <div class="modal-header">
+                <div>
+                    <h3>${item.name}</h3>
+                    <p class="coop-panel--city">${item.city}, ${window.AppData.countries[item.country]}</p>
+                </div>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+            </div>
+            <div class="modal-body">
+              <ul class="modal-sidebar">
+                  <li class="modal-sidebar-item"><a target="_blank" href="${item.url}"><i class="fa fa-link"></i>${item.url}</a></li>
+                  <li class="modal-sidebar-item"><a target="_blank" href="mailto:${item.mail}"><i class="fa fa-envelope"></i>${item.mail}</a></li>
+                  ${ item.facebook_url ? `<li class="modal-sidebar-item"><a target="_blank" href="${item.facebook_url}"><i class="fa fa-facebook"></i>${item.facebook_name ? item.facebook_name : item.name}</li></a>`: ""}
+                  ${ item.twitter_url ? `<li class="modal-sidebar-item"><a target="_blank" href="${item.twitter_url}"><i class="fa fa-twitter"></i>${item.twitter_handle}</li></a>` : ""}
+              </ul>
+              ${ text ? `<div class="modal-not-sidebar">${text}</div>` : ""}
+            </div>
+          </div>
+        `)
+        $('#coop-modal').modal()
+      })
+      markers.push(marker)
+    }
+  )
 
-    // center map
-    let group = new L.featureGroup(markers)
-    map.fitBounds(group.getBounds().pad(0.25))
+  // center map
+  let group = new L.featureGroup(markers)
+  map.fitBounds(group.getBounds().pad(0.10))
+
+  // side panels related code
+  $('.side-panel-example-close').on('click', function() {
+    $('.side-panel-example-close').closest('#side-panel-example').removeClass("show")
+  })
+
 }
 
-document.getElementById('show-more-services').addEventListener('click', function (e) {
-  event.preventDefault(e)
-  document.getElementById('more-services').style.display = ''
-  fadeIn(document.getElementById('more-services'))
-  document.querySelectorAll("[href='#show-more-services']")[0].style.display = 'none'
-})
+if (document.getElementById('show-more-services')) {
+  document.getElementById('show-more-services').addEventListener('click', function(e) {
+    event.preventDefault(e)
+    document.getElementById('more-services').style.display = ''
+    fadeIn(document.getElementById('more-services'))
+    document.querySelectorAll("[href='#show-more-services']")[0].style.display = 'none'
+  })
+}
 
 
 // Map helpers
@@ -60,13 +86,16 @@ document.getElementById('show-more-services').addEventListener('click', function
 function init(id, center, zoom = 13, zoomControl = true) {
 
   if (!center && window.AppData && window.AppData.MapHelper && window.AppData.MapHelper.center) {
-    let [ latitude, longitude ] = window.AppData.MapHelper.center.split(',')
+    let [latitude, longitude] = window.AppData.MapHelper.center.split(',')
     if (latitude && longitude) {
-      center = [ parseFloat(latitude), parseFloat(longitude) ]
+      center = [parseFloat(latitude), parseFloat(longitude)]
     }
   }
 
-  var map = L.map(id, { scrollWheelZoom: false, zoomControl })
+  var map = L.map(id, {
+    scrollWheelZoom: false,
+    zoomControl
+  })
 
   if (center && zoom) {
     map.setView(center, zoom)
@@ -79,4 +108,3 @@ function init(id, center, zoom = 13, zoomControl = true) {
 
   return map
 }
-
