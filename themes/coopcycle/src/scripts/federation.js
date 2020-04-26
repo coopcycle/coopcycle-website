3202952d9faa984d4c2eeb818a50243889c37922
Alexandre Segura <mex.zktk@gmail.com>
2020-04-26 13:28:42 +0200

    Simplify code, use ES6 imports.

--- themes/coopcycle/src/scripts/federation.js
+++ themes/coopcycle/src/scripts/federation.js
@@ -1,4 +1,5 @@
 import 'core-js'
+import L from 'leaflet'
 
 function fadeIn(el) {
   el.style.opacity = 0;
@@ -20,46 +21,43 @@ if (document.getElementById('coops-map')) {
 
   $('.map-wrapper').fadeIn()
 
-  let L = require('leaflet'),
-    markers = [],
-    marker
-
   var map = L.map('coops-map', { scrollWheelZoom: false })
 
   L.tileLayer('https://cartodb-basemaps-{s}.global.ssl.fastly.net/rastertiles/voyager/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>, &copy;<a href="https://carto.com/attribution">CARTO</a>'
   }).addTo(map)
 
-  window.AppData.coops.forEach(
-    function(item) {
-      marker = L.marker([item.latitude, item.longitude]).addTo(map).on('click', function() {
-        // NB : the content will be indexed thanks to the 'no-desktop' div which contains the links to the external pages
-        // fill in modal with coop data
-        let text = item.text ? (item.text[window.AppData.lang] || item.text['en']) : ''
-        $('#coop-modal .modal-content').html(`
-            <div class="modal-header">
-                <div>
-                    <h3>${item.name}</h3>
-                    <p class="coop-panel--city">${item.city}, ${window.AppData.countries[item.country]}</p>
-                </div>
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-            </div>
-            <div class="modal-body">
-              <ul class="modal-sidebar">
-                  ${ item.url ? `<li class="modal-sidebar-item"><a target="_blank" href="${item.url}"><i class="fa fa-link"></i>${item.url}</a></li>` : '' }
-                  <li class="modal-sidebar-item"><a target="_blank" href="mailto:${item.mail}"><i class="fa fa-envelope"></i>${item.mail}</a></li>
-                  ${ item.facebook_url ? `<li class="modal-sidebar-item"><a target="_blank" href="${item.facebook_url}"><i class="fa fa-facebook"></i>${item.facebook_name ? item.facebook_name : item.name}</li></a>`: ""}
-                  ${ item.twitter_url ? `<li class="modal-sidebar-item"><a target="_blank" href="${item.twitter_url}"><i class="fa fa-twitter"></i>${item.twitter_handle}</li></a>` : ""}
-              </ul>
-              ${ text ? `<div class="modal-not-sidebar">${text}</div>` : ""}
-            </div>
+  const markers = window.AppData.coops.map(item => {
+
+    return L.marker([item.latitude, item.longitude]).on('click', function() {
+      // NB : the content will be indexed thanks to the 'no-desktop' div which contains the links to the external pages
+      // fill in modal with coop data
+      let text = item.text ? (item.text[window.AppData.lang] || item.text['en']) : ''
+      $('#coop-modal .modal-content').html(`
+          <div class="modal-header">
+              <div>
+                  <h3>${item.name}</h3>
+                  <p class="coop-panel--city">${item.city}, ${window.AppData.countries[item.country]}</p>
+              </div>
+              <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
           </div>
-        `)
-        $('#coop-modal').modal()
-      })
-      markers.push(marker)
-    }
-  )
+          <div class="modal-body">
+            <ul class="modal-sidebar">
+                ${ item.url ? `<li class="modal-sidebar-item"><a target="_blank" href="${item.url}"><i class="fa fa-link"></i>${item.url}</a></li>` : '' }
+                <li class="modal-sidebar-item"><a target="_blank" href="mailto:${item.mail}"><i class="fa fa-envelope"></i>${item.mail}</a></li>
+                ${ item.facebook_url ? `<li class="modal-sidebar-item"><a target="_blank" href="${item.facebook_url}"><i class="fa fa-facebook"></i>${item.facebook_name ? item.facebook_name : item.name}</li></a>`: ""}
+                ${ item.twitter_url ? `<li class="modal-sidebar-item"><a target="_blank" href="${item.twitter_url}"><i class="fa fa-twitter"></i>${item.twitter_handle}</li></a>` : ""}
+            </ul>
+            ${ text ? `<div class="modal-not-sidebar">${text}</div>` : ""}
+          </div>
+        </div>
+      `)
+      $('#coop-modal').modal()
+    })
+
+  })
+
+  markers.forEach(marker => marker.addTo(map))
 
   // center map
   let group = new L.featureGroup(markers)
