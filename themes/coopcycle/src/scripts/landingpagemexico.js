91c75fe7d2b2f678cdf7b79ab467ad8b698371e8
AniaCuapio <ania.cuapio@gmail.com>
2022-02-03 05:19:06 -0600

    card added

--- themes/coopcycle/src/scripts/landingpagemexico.js
+++ themes/coopcycle/src/scripts/landingpagemexico.js
@@ -3,6 +3,14 @@ import _ from 'lodash'
 
 window._paq = window._paq || []
 
+const coops = data.coops.map(item => {item.country("mx")
+}
+
+console.log (coops)
+
+coops.forEach(coop=>card(coop))
+
+
 $('#city-dropdown').on('change', function(e) {
   if (!_.isEmpty($(this).val())) {
     window._paq.push(['trackEvent', 'Homepage', 'selectCity', $(this).find('option:selected').text()])
